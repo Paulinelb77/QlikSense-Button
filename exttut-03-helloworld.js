ce39ab5e5038a5c58457ef86cd6bf6a2b1db4ad2
Paulinelb77 <pauline.leboucher@keyrus.com>
2017-04-21 09:49:54 +0200

    Update exttut-03-helloworld.js

--- exttut-03-helloworld.js
+++ exttut-03-helloworld.js
@@ -1,18 +1,24 @@
-define( [
-        'jquery'
-    ],
-    function ( $ ) {
-        'use strict';
-
-        return {
-
-            //Paint resp.Rendering logic
-            paint: function ( $element, layout ) {
-
-                var $helloWorld = $( document.createElement( 'div' ) );
-                $helloWorld.html( 'Hello World from the extension "01-ExtTut-HelloWorld"<br/>' );
-                $element.append( $helloWorld );
-
-            }
-        };
-    } );
+define(["qlik", "./util", "./properties"], function (qlik, util, prop) {
+    'use strict';
+        
+    function getClass(style, type, selected) {
+        switch (style) {
+            case "material":
+            case "bootstrap":
+                if (selected) {
+                    return "selected";
+                }
+                break;
+            default:
+                switch (type) {
+                    case 'button':
+                        return selected ? 'qui-button-selected lui-button lui-button--success' : 'qui-button lui-button';
+                    case 'select':
+                        return 'qui-select lui-select';
+                    case 'input':
+                        return 'qui-input lui-input';
+                }
+        }
+    }
+        
+} );
